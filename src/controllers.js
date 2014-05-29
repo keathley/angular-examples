86c8e54ac0c55717ebce1b4589ea7f1e09317720
Chris Keathley <spyc3r@gmail.com>
2014-05-28 21:41:47 -0400

    Fix ngroute loading

--- src/controllers.js
+++ src/controllers.js
@@ -1,18 +1,18 @@
 // Create module
-var aMailServices = angular.module('AMail', []);
+var aMailServices = angular.module('AMail', ['ngRoute']);
 
 //configure routing
-aMailServices.config(function($routeProvider) 
+aMailServices.config(function($routeProvider)
 {
   $routeProvider.
-  when('/', 
+  when('/',
   {
     controller: 'ListController',
     templateUrl: 'list.html'
   }).
 // Notice that for the detail view, we specify a parameterized URL component
 // by placing a colon in front of the id
-  when('/view/:id', 
+  when('/view/:id',
   {
     controller: 'DetailController',
     templateUrl: 'detail.html'
@@ -36,20 +36,20 @@ aMailServices.factory('Messages', function()
                 date: 'Dec 7, 2013 12:32:00', recipients: ['greg@somecompany.com'],
                 message: 'Hey, we should get together for lunch sometime and catch up.'
                 +'There are many things we should collaborate on this year.'
-              }, 
+              },
               {
                 id: 1,  sender: 'maria@somecompany.com',
                 subject: 'Where did you leave my laptop?',
                 date: 'Dec 7, 2013 8:15:12', recipients: ['greg@somecompany.com'],
                 message: 'I thought you were going to put it in my desk drawer.'
                 +'But it does not seem to be there.'
-              }, 
+              },
               {
                 id: 2, sender: 'bill@somecompany.com', subject: 'Lost python',
                 date: 'Dec 6, 2013 20:35:02', recipients: ['greg@somecompany.com'],
                 message: 'Nobody panic, but my pet python is missing from her cage.'
                 +'She doesn\'t move too fast, so just call me if you see her.'
-              } 
+              }
             ];
   };
 
@@ -60,7 +60,7 @@ aMailServices.factory('Messages', function()
 // add ListController to module
 aMailServices.controller('ListController', function($scope, Messages)
 {
-    $scope.messages = Messages.query();  
+    $scope.messages = Messages.query();
 });
 
 //add DetailController to module
@@ -69,6 +69,6 @@ aMailServices.controller('ListController', function($scope, Messages)
 aMailServices.controller('DetailController', function($scope, $routeParams, Messages)
 {
     $scope.messages = Messages.query();
-    $scope.message = $scope.messages[$routeParams.id];  
+    $scope.message = $scope.messages[$routeParams.id];
 });
 
